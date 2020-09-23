73be2df2476bc3da3cfc92e0bd297d01c48a7072
André Luis Francisco Junior <33641043+Andre-LFJ@users.noreply.github.com>
2020-09-23 02:32:57 -0300

    Update bruta.h

--- bruta.h
+++ bruta.h
@@ -2,4 +2,12 @@
 #include"main_header.h"
 using namespace std;
 
-void fun3(void);
+void fun3(void); // não foi usada, pode apagar (?)
+
+bool forcabruta(mpz_t possivelPrimo);   // checa de 3 até a raiz do número
+                                        // retorna true se for primo
+                                        // retorna false se nao for primo
+
+bool forcabruta_ruim(mpz_t possivelPrimo);  // checa de 3 até a metade do número
+                                            // retorna true se for primo
+                                            // retorna false se nao for primo
