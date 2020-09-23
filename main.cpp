9e222d09b136a537f29e2787ca5484cb5c1297ef
Miguel Nunes <miguel.alfredo.nunes@gmail.com>
2020-09-23 17:26:57 -0300

    menuzinho da main

--- main.cpp
+++ main.cpp
@@ -4,49 +4,88 @@
 #include"bruta.h"
 
 using namespace std;
-/*
-####################
-Ideias:
-    
-####################
-*/
+
 int main(int argc, char const *argv[]){ // arrumar, ver aula 31/08
-    if(argc == 3){
+
+	TOTALBITS = -1;
+	FILENAME = "-";
+
+	if(argc == 3){
 		TOTALBITS = atoi(argv[1]);
-    }else if(argc == 2){
+		FILENAME.assign(argv[2]);
+	}else if(argc == 2){
 		TOTALBITS = atoi(argv[1]);
-	}else{
-        cout << "Digite o número de bits:" << endl;
-        cin >> TOTALBITS;
-    }
-    
-	string Filename;
-    mpz_t E, N, D, PQ;//, inverso;
-    mpz_init2(E,TOTALBITS);
-    mpz_init2(N,TOTALBITS);
-    mpz_init2(D,TOTALBITS);
-	mpz_init2(PQ, TOTALBITS);
-	//mpz_init2(inverso, TOTALBITS);
+	}
 
-	Filename = "file.txt";
+	int opcao = 0;
+	mpz_t E, N, D, PQ;
 
-    chave_publica(E,N,PQ); // chave publica = <E,N>
-    chave_privada(D,E,PQ); // chave privada = <D,N>
 
-	//if(mpz_invert(inverso, E, PQ) != 0){
-	//	gmp_printf("Inverso Modular de E: %Zd\n",inverso);	
-	//}
-	//if(mpz_invert(inverso, D, PQ) != 0){
-	//	gmp_printf("Inverso Modular de D: %Zd\n",inverso);	
-	//} 
-	// sanity check
+	while(true){
+		cout << "Selecione a operação: " << endl;
+		cout << "\t[1] Gerar Chave Pública " << endl;
+		cout << "\t[2] Gerar Chave Privada " << endl;
+		cout << "\t[3] Criptografar um Arquivo " << endl;
+		cout << "\t[4] Descriptografar um Arquivo " << endl;
+		cout << "\t[5] Quebra de Chave por Força Bruta " << endl;
+		cout << "\t[6] Sair " << endl;
+		cin >> opcao;
 
-	gmp_printf("Chave Pública (E,N): <%Zd, %Zd>\n",E,N);
-	gmp_printf("Chave Privada (D,N): <%Zd, %Zd>\n",D,N);
-	gmp_printf("(P-1)(Q-1): %Zd\n",PQ);
+		switch(opcao){
+			case 1:
+				if(TOTALBITS == -1){
+					cout << "Digite o número de bits:" << endl;
+					cin >> TOTALBITS;
+					mpz_init2(E,TOTALBITS);
+					mpz_init2(N,TOTALBITS);
+					mpz_init2(D,TOTALBITS);
+					mpz_init2(PQ, TOTALBITS);
+				}
+				chave_publica(E,N,PQ); // chave publica = <E,N>
+				gmp_printf("Chave Pública (E,N): <%Zd, %Zd>\n",E,N);
+			break;
 
-	criptografar_arquivo(E,N,Filename);
-	descriptografar_arquivo(D,N,Filename);
+			case 2:
+				if(TOTALBITS == -1){
+					cout << "Digite o número de bits:" << endl;
+					cin >> TOTALBITS;
+					mpz_init2(E,TOTALBITS);
+					mpz_init2(N,TOTALBITS);
+					mpz_init2(D,TOTALBITS);
+					mpz_init2(PQ, TOTALBITS);
+				}
+				if(mpz_cmp_ui(E,0) == 0){
+					cout << "Gerando a chave pública..." << endl;
+					chave_publica(E,N,PQ); // chave publica = <E,N>
+					gmp_printf("Chave Pública (E,N): <%Zd, %Zd>\n",E,N);
+				}
+				chave_privada(D,E,PQ); // chave privada = <D,N>
+				gmp_printf("Chave Privada (D,N): <%Zd, %Zd>\n",D,N);
+			break;
+
+			case 3:
+				if(FILENAME == "-"){
+					cout << "Digite o nome do arquivo sem a extensão .txt:" << endl;
+					cin >> FILENAME;
+				}
+				FILENAME += ".txt";
+				cout << "Criptografando arquivo " << FILENAME << " ..." << endl;
+				criptografar_arquivo(E,N);
+			break;
+
+			case 4:
+				cout << "Descriptografando arquivo CRIPTO-" << FILENAME << " ..." << endl;
+				descriptografar_arquivo(D,N);
+			break;
+
+			case 6:
+				return 0;
+			break;
+		}
+	}
+
+	//gmp_printf("(P-1)(Q-1): %Zd\n",PQ);
+
 
     return 0;
 }
